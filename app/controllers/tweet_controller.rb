14618826d5ea28a206ef9a9dee6257b079122bdd
Santiago Castillo <sancastillog@gmail.com>
2021-03-12 01:22:02 -0500

    create tweets, register user, login and db follows

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,4 +1,24 @@
 class TweetController < ApplicationController
     def index
     end
+
+    def new
+        @tweet = Tweet.new
+    end
+    
+    def create
+        @tweet = Tweet.new(tweet_params)
+        if @tweet.save
+            redirect_to(@tweet)
+        else
+            render :new
+        end
+    end
+    
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:content, :user_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  resources :users do
+    resources :tweet
+  end
+
   root 'welcome#index'
 end
